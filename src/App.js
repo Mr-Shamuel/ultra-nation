630bf21dcfe29e77548e0f017ffdecccecdea4ff
Your Name <you@example.com>
2021-08-13 17:45:25 +0600

    passing data to a child component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Country from './Component/Country/Country';
 
 function App() {
+
+const [countries,setCountries] = useState([]);
+useEffect(()=>{
+  fetch('https://restcountries.eu/rest/v2/all')
+.then(res=>res.json())
+.then(data=>{
+  setCountries(data); 
+})
+.catch(error=>console.log(error));
+
+},[])
+console.log(countries);
+
   return (
     <div className="App">
+      <h1>Total Countries: {countries.length}</h1>
+      <ul>
+        {
+          
+          countries.map(x=><Country countyName={x.name}></Country>)
+        }
+      </ul>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/Component/Country/Country.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Country = (props) => {
+
+    return (
+        <div>
+            <h1>This is {props.countyName}</h1>  
+        </div>
+    );
+};
+
+export default Country;
